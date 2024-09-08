df824035236971a74550982362e18e6196d99339
Enes <enesyerebakan7@gmail.com>
2024-09-09 02:10:02 +0300

    Movie edit

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title:{
         type: String,
-        required: true
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır '],
+		minlength: [1, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlength: 30,
+        minlength:1
+    },
+    country: {
+        type: String,
+        minlength:1,
+        maxlength:30
+    },
+    year: {
+        type: Number,
+        max:2040,
+        min:1850,
+    },
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now
